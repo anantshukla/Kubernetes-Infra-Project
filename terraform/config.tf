062a221f1911bf7bbd63aead00595e163f0289c4
Anant Shukla <accounts@anantshukla.com>
2024-01-12 09:57:14 -0500

    Added secrets

---
+++ terraform/config.tf
@@ -0,0 +1,11 @@
+terraform {
+    backend "gcs" { 
+      bucket  = "terraform-state-k8s-playfield"
+      prefix  = "prod"
+    }
+}
+
+provider "google" {
+  project = "k8s-playfield"
+  region = "us-west1"
+}
